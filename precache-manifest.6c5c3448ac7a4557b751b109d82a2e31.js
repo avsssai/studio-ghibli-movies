a408f3ff56b0956f46f7352c1e3e0216dca12700
avsssai <34810623+avsssai@users.noreply.github.com>
2019-06-10 15:09:25 +0530

    Updates

---
+++ precache-manifest.6c5c3448ac7a4557b751b109d82a2e31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6466044a578243e909d79b7506d25193",
+    "url": "/studio-ghibli-movies/index.html"
+  },
+  {
+    "revision": "9fe86370f75c4a23c7a2",
+    "url": "/studio-ghibli-movies/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "051557fa4f586033110a",
+    "url": "/studio-ghibli-movies/static/css/main.66afe323.chunk.css"
+  },
+  {
+    "revision": "9fe86370f75c4a23c7a2",
+    "url": "/studio-ghibli-movies/static/js/2.fd5f17ab.chunk.js"
+  },
+  {
+    "revision": "051557fa4f586033110a",
+    "url": "/studio-ghibli-movies/static/js/main.a25d02ad.chunk.js"
+  },
+  {
+    "revision": "2d1daac2f9dd4ad9c4fb",
+    "url": "/studio-ghibli-movies/static/js/runtime~main.a1bdb3d3.js"
+  }
+]);
